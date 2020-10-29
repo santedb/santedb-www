b536c30240324ad0652465153be67d9b5b45b741
Justin Fyfe <justin@fyfesoftware.ca>
2020-10-29 18:33:25 -0400

    Updated versions'

--- santedb-www/Properties/AssemblyInfo.cs
+++ santedb-www/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyInformationalVersion("Langley")]
-[assembly: AssemblyVersion("2.0.43.*")]
-[assembly: AssemblyFileVersion("2.0.43.0")]
+[assembly: AssemblyVersion("2.0.45.*")]
+[assembly: AssemblyFileVersion("2.0.45.0")]
