cc7e8430f5dbc6d0d794bf6b3b5cfe5f4d0476ca
Justin Fyfe <justin@fyfesoftware.ca>
2020-11-29 13:06:25 -0500

    Updated refs

--- santedb-www/Properties/AssemblyInfo.cs
+++ santedb-www/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("Langley")]
-[assembly: AssemblyVersion("2.0.48.*")]
-[assembly: AssemblyFileVersion("2.0.48.0")]
+[assembly: AssemblyInformationalVersion("Montreal")]
+[assembly: AssemblyVersion("2.0.56.*")]
+[assembly: AssemblyFileVersion("2.0.56.0")]
