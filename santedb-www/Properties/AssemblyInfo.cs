3c5cf8f437759f10b09a6ca904aababd454f481a
Justin Fyfe <justin@fyfesoftware.ca>
2021-08-04 11:13:11 -0400

    Rev'd versions

--- santedb-www/Properties/AssemblyInfo.cs
+++ santedb-www/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyInformationalVersion("Ottawa")]
-[assembly: AssemblyVersion("2.1.35.*")]
-[assembly: AssemblyFileVersion("2.1.35.0")]
+[assembly: AssemblyVersion("2.1.48.*")]
+[assembly: AssemblyFileVersion("2.1.48.0")]
