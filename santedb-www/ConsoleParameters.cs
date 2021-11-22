c236cbd6f6c43759651e01f291c412a47525c9b1
Justin Fyfe <justin@fyfesoftware.ca>
2021-11-22 12:27:29 -0500

    Revert "Removed the binaries"
    This reverts commit 510f65b2ae2e6f518b981aa8a0aa2557ef239fac.

---
+++ santedb-www/ConsoleParameters.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * Portions Copyright 2015-2019 Mohawk College of Applied Arts and Technology
+ * Portions Copyright 2019-2019 SanteSuite Contributors (See NOTICE)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * User: Justin Fyfe
+ * Date: 2019-8-8
+ */
+
+using MohawkCollege.Util.Console.Parameters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace santedb_www
+{
+    /// <summary>
+    /// Represents console parameters for this particular instance
+    /// </summary>
+    public class ConsoleParameters
+    {
+        // <summary>
+        /// When true, parameters should be shown
+        /// </summary>
+        [Description("Shows help and exits")]
+        [Parameter("?")]
+        [Parameter("help")]
+        public bool ShowHelp { get; set; }
+
+        /// <summary>
+        /// When true console mode should be enabled
+        /// </summary>
+        [Description("Instructs the host process to run in console mode")]
+        [Parameter("c")]
+        [Parameter("console")]
+        public bool ConsoleMode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the instance that is running
+        /// </summary>
+        [Description("Identifies the name of the instance to start")]
+        [Parameter("n")]
+        [Parameter("name")]
+        public string InstanceName { get; set; }
+
+        /// <summary>
+        /// Installs the service
+        /// </summary>
+        [Description("Installs the service")]
+        [Parameter("i")]
+        [Parameter("install")]
+        public bool Install { get; set; }
+
+        /// <summary>
+        /// Installs the service
+        /// </summary>
+        [Description("Uninstalls the service")]
+        [Parameter("u")]
+        [Parameter("uninstall")]
+        public bool Uninstall { get; set; }
+
+        /// <summary>
+        /// Reset the service installation
+        /// </summary>
+        [Description("Resets the configuration of this WWW instance to default")]
+        [Parameter("r")]
+        [Parameter("reset")]
+        public bool Reset { get; set; }
+
+        /// <summary>
+        /// Set the application name
+        /// </summary>
+        [Description("Sets the identity of the application (for OAUTH) for this instance")]
+        [Parameter("appname")]
+        public String ApplicationName { get; set; }
+
+        /// <summary>
+        /// The application secret
+        /// </summary>
+        [Description("Sets the secret of the application (for OAUTH) for this instance")]
+        [Parameter("appsecret")]
+        public String ApplicationSecret { get; set; }
+
+        /// <summary>
+        /// Start in noninteractive method
+        /// </summary>
+        [Description("Do not quit when the ENTER key is pressed")]
+        [Parameter("daemon")]
+        public bool Forever { get; internal set; }
+
+        /// <summary>
+        /// Force loading of DLLs
+        /// </summary>
+        [Description("Force the loading of DLLs (useful on some Linux distros)")]
+        [Parameter("dllForce")]
+        public bool Force { get; set; }
+
+        /// <summary>
+        /// Restart the specifie instance
+        /// </summary>
+        [Description("Restart the running service control")]
+        [Parameter("restart")]
+        public bool Restart { get; internal set; }
+    }
+}
---
+++ santedb-www/ServiceInstaller.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Portions Copyright 2015-2019 Mohawk College of Applied Arts and Technology
+ * Portions Copyright 2019-2019 SanteSuite Contributors (See NOTICE)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * User: Justin Fyfe
+ * Date: 2019-8-8
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace santedb_www
+{
+    [RunInstaller(true)]
+    public class ServiceInstaller : System.Configuration.Install.Installer
+    {
+        /// <summary>
+		/// The m service process installer.
+		/// </summary>
+		private System.ServiceProcess.ServiceProcessInstaller m_serviceProcessInstaller;
+
+        /// <summary>
+        /// The m service installer.
+        /// </summary>
+        private System.ServiceProcess.ServiceInstaller m_serviceInstaller;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceInstaller"/> class.
+        /// </summary>
+        public ServiceInstaller(string instanceName)
+        {
+            // This call is required by the Designer.
+            InitializeComponent(instanceName);
+        }
+
+        /// <summary>
+        /// Initializes the component.
+        /// </summary>
+        private void InitializeComponent(string instanceName)
+        {
+            this.m_serviceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.m_serviceInstaller = new System.ServiceProcess.ServiceInstaller();
+
+            this.m_serviceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.m_serviceProcessInstaller.Password = null;
+            this.m_serviceProcessInstaller.Username = null;
+            this.m_serviceInstaller.ServiceName = $"sdb-www-{instanceName}";
+            this.m_serviceInstaller.Description = "Provides the execution environment for SanteDB";
+            this.m_serviceInstaller.DelayedAutoStart = true;
+            this.m_serviceInstaller.DisplayName = $"SanteDB Disconnected Client ({instanceName})";
+            this.m_serviceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+
+            this.Installers.AddRange(
+                new System.Configuration.Install.Installer[]
+                {
+                    this.m_serviceProcessInstaller,
+                    this.m_serviceInstaller
+                });
+        }
+    }
+}
