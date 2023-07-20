87a7c443c139ff0556c7def93e609b1b6258d189
Trevor Davis <trevor@trevordavis.me>
2023-07-20 07:20:53 -0400

    BuildBot: Added release version

--- santedb-www/Properties/Version.cs
+++ santedb-www/Properties/Version.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyFileVersion("3.0.990")]
-[assembly: System.Reflection.AssemblyVersion("3.0.990")]
+[assembly: System.Reflection.AssemblyFileVersion("3.0.890")]
+[assembly: System.Reflection.AssemblyVersion("3.0.890")]
 
 
