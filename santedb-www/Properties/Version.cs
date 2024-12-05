bb6835f8f82b267457a33e32d09b50ba9ca2aca4
Justin Fyfe <justin@fyfesoftware.ca>
2024-12-05 10:55:31 -0500

    BuildBot: Added release version

--- santedb-www/Properties/Version.cs
+++ santedb-www/Properties/Version.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyFileVersion("3.0.1991")]
-[assembly: System.Reflection.AssemblyVersion("3.0.1991")]
+[assembly: System.Reflection.AssemblyFileVersion("3.0.1992")]
+[assembly: System.Reflection.AssemblyVersion("3.0.1992")]
 
 
